9df759f184692d4e5c73f1485cc01e3f9e35ae93
Tim Nelson <traveltimn@gmail.com>
2019-10-25 14:45:18 +0100

    JavaScript30

--- day-04/script.js
+++ day-04/script.js
@@ -17,26 +17,33 @@ document.addEventListener("DOMContentLoaded", function() {
     const people = ["Beck, Glenn", "Becker, Carl", "Beckett, Samuel", "Beddoes, Mick", "Beecher, Henry", "Beethoven, Ludwig", "Begin, Menachem", "Belloc, Hilaire", "Bellow, Saul", "Benchley, Robert", "Benenson, Peter", "Ben-Gurion, David", "Benjamin, Walter", "Benn, Tony", "Bennington, Chester", "Benson, Leana", "Bent, Silas", "Bentsen, Lloyd", "Berger, Ric", "Bergman, Ingmar", "Berio, Luciano", "Berle, Milton", "Berlin, Irving", "Berne, Eric", "Bernhard, Sandra", "Berra, Yogi", "Berry, Halle", "Berry, Wendell", "Bethea, Erin", "Bevan, Aneurin", "Bevel, Ken", "Biden, Joseph", "Bierce, Ambrose", "Biko, Steve", "Billings, Josh", "Biondo, Frank", "Birrell, Augustine", "Black, Elk", "Blair, Robert", "Blair, Tony", "Blake, William"];
     // console.table(people);
 
-    // Filter the list of inventors for those who were born  in the 1500's
-    const fifteensDiv = document.getElementById("fifteens");
-    const fifteens = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
-    const table = document.createElement("TABLE");
-    const tableRowHead = document.createElement("TR");
-    const tableHeader1 = document.createElement("TH"); tableHeader1.innerHTML = "First"; tableRowHead.appendChild(tableHeader1);
-    const tableHeader2 = document.createElement("TH"); tableHeader2.innerHTML = "Last"; tableRowHead.appendChild(tableHeader2);
-    const tableHeader3 = document.createElement("TH"); tableHeader3.innerHTML = "Birth"; tableRowHead.appendChild(tableHeader3);
-    const tableHeader4 = document.createElement("TH"); tableHeader4.innerHTML = "Death"; tableRowHead.appendChild(tableHeader4);
-    table.appendChild(tableRowHead);
-    fifteensDiv.appendChild(table);
-    fifteens.forEach(function (info) {
-        const tableRowBody = document.createElement("TR");
-        const tableData1 = document.createElement("TD"); tableData1.innerHTML = info.first; tableRowBody.appendChild(tableData1);
-        const tableData2 = document.createElement("TD"); tableData2.innerHTML = info.last; tableRowBody.appendChild(tableData2);
-        const tableData3 = document.createElement("TD"); tableData3.innerHTML = info.year; tableRowBody.appendChild(tableData3);
-        const tableData4 = document.createElement("TD"); tableData4.innerHTML = info.passed; tableRowBody.appendChild(tableData4);
-        table.appendChild(tableRowBody);
+    // FILTER the list of inventors for those who were born  in the 1500's
+    const filterDIV = document.getElementById("filter");
+    const filterData = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+    const filterTable = document.createElement("TABLE");
+    const filterTRhead = document.createElement("TR");
+    const filterTH1 = document.createElement("TH"); filterTH1.innerHTML = "First"; filterTRhead.appendChild(filterTH1);
+    const filterTH2 = document.createElement("TH"); filterTH2.innerHTML = "Last"; filterTRhead.appendChild(filterTH2);
+    const filterTH3 = document.createElement("TH"); filterTH3.innerHTML = "Birth"; filterTRhead.appendChild(filterTH3);
+    const filterTH4 = document.createElement("TH"); filterTH4.innerHTML = "Death"; filterTRhead.appendChild(filterTH4);
+    filterTable.appendChild(filterTRhead);
+    filterDIV.appendChild(filterTable);
+    filterData.forEach(info => {
+        const filterTRbody = document.createElement("TR");
+        const filterTD1 = document.createElement("TD"); filterTD1.innerHTML = info.first; filterTRbody.appendChild(filterTD1);
+        const filterTD2 = document.createElement("TD"); filterTD2.innerHTML = info.last; filterTRbody.appendChild(filterTD2);
+        const filterTD3 = document.createElement("TD"); filterTD3.innerHTML = info.year; filterTRbody.appendChild(filterTD3);
+        const filterTD4 = document.createElement("TD"); filterTD4.innerHTML = info.passed; filterTRbody.appendChild(filterTD4);
+        filterTable.appendChild(filterTRbody);
     });
 
-    //
+    // MAP an array of the inventors' first and last names
+    const fullNames= inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+    const mapDIV = document.getElementById("map");
+    const mapUL = document.createElement("UL");
+    fullNames.forEach(name => {
+        const mapLI = document.createElement("LI"); mapLI.innerHTML = name; mapUL.appendChild(mapLI);
+    });
+    mapDIV.appendChild(mapUL);
 
 });

6fdc5f9ad39b6997dca9363d43e78d8e5d07ce91
Tim Nelson <traveltimn@gmail.com>
2019-10-26 10:51:11 +0100

    JavaScript30

--- day-05/script.js
+++ day-05/script.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    //
+    const panels = document.querySelectorAll(".panel");
+    function toggleOpen() {
+        this.classList.toggle("open");
+    }
+    function toggleActive(event) {
+        if (event.propertyName.includes("flex")) {
+            this.classList.toggle("open-active");
+        }
+    }
+    panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+    panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
 });

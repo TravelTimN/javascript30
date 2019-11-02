5e29999e048484b8e9747501554468ae505e42e6
Tim Nelson <traveltimn@gmail.com>
2019-11-02 10:53:31 +0000

    JavaScript30

--- day-11/script.js
+++ day-11/script.js
@@ -1,5 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
-    
+    // Get our Elements
+    const player = document.querySelector(".player");
+    const video = player.querySelector(".viewer");
+    const progress = player.querySelector(".progress");
+    const progressBar = player.querySelector(".progress-filled");
+    const toggle = player.querySelector(".toggle");
+    const skipButtons = player.querySelectorAll("[data-skip]");
+    const ranges = player.querySelectorAll(".player-slider");
 
-});
+
+
+    // Build our Functions
+    function togglePlay() {
+        // if video is paused, toggle play and vice versa
+        const method = video.paused ? "play": "pause";
+        video[method]();
+        
+    }
+
+    function updateButton() {
+        const icon = this.paused ? "►" : "❚ ❚";
+        toggle.textContent = icon;
+    }
+
+    function skip() {
+        video.currentTime += parseFloat(this.dataset.skip);
+    }
+
+
+    // Hook-up the Event Listeners
+    video.addEventListener("click", togglePlay);
+    video.addEventListener("play", updateButton);
+    video.addEventListener("pause", updateButton);
+    toggle.addEventListener("click", togglePlay);
+    skipButtons.forEach(button => {
+        button.addEventListener("click", skip);
+    });
+
+// });

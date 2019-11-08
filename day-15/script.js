80c72a3328adc282263b988bd0f2e03de77ae2b4
Tim Nelson <traveltimn@gmail.com>
2019-11-08 21:01:57 +0000

    JavaScript30

---
+++ day-15/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const addItems = document.querySelector(".add-items");
+    const itemsList = document.querySelector(".plates");
+    const items = [];
+
+});

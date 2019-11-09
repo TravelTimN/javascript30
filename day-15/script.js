4b0edd8c590bdb278a84bf7ec8c1143ebfeafaa2
Tim Nelson <traveltimn@gmail.com>
2019-11-09 13:51:38 +0000

    JavaScript30

--- day-15/script.js
+++ day-15/script.js
@@ -1,24 +1,27 @@
-// document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
     const addItems = document.querySelector(".add-items");
     const itemsList = document.querySelector(".plates");
-    const items = [];
+    const items = JSON.parse(localStorage.getItem("items")) || []; // existing localStorage items, or blank []
 
 
     function addItem(e) {
-        e.preventDefault();
-        const text = (this.querySelector("[name=item]")).value;
+        e.preventDefault(); // stop page from reloading
+        const text = (this.querySelector("[name=item]")).value; // get item name from input
         const item = {
             // text: text,
-            text,
+            text, // shorthand ES6 of same exact line above (text: text)
             done: false
         }
-        items.push(item);
-        populateList(items, itemsList);
-        this.reset();
+        items.push(item); // push the item into 'items[]'
+        populateList(items, itemsList); // visually update the checkbox
+        // localStorage.setItem("items", items); // returns [object Object] in Application>LocalStorage
+        localStorage.setItem("items", JSON.stringify(items)); // localStorage only accepts 'strings'
+        this.reset(); // reset the input to blank
     }
 
     function populateList(plates = [], platesList) {
+        // add new <li> item with <input> + <label> for each item pushed to items[]
         platesList.innerHTML = plates.map((plate, i) => {
             return `
                 <li>
@@ -29,6 +32,17 @@
         }).join("");
     }
 
-    addItems.addEventListener("submit", addItem);
+    function toggleDone(e) {
+        if (!e.target.matches("input")) return; // skip this unless it's an <input> element
+        const el = e.target; // <input> is the event target
+        const index = el.dataset.index; // index is the data-index value
+        items[index].done = !items[index].done; // toggle boolean of 'done'
+        localStorage.setItem("items", JSON.stringify(items)); // store it in localStorage
+        populateList(items, itemsList); // visually update the checkbox
+    }
 
-// });
+    addItems.addEventListener("submit", addItem); // on 'submit', add value of input
+    itemsList.addEventListener("click", toggleDone); // on 'click', toggle checkbox
+    populateList(items, itemsList); // build list of <li> elements from localStorage or new items
+
+});

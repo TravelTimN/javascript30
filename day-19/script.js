f26f2f4b509cbcde221dc5fd360cbeccf01b5204
Tim Nelson <traveltimn@gmail.com>
2019-11-13 20:37:25 +0000

    JavaScript30

---
+++ day-19/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const video = document.querySelector(".player");
+    const canvas = document.querySelector(".photo");
+    const ctx = canvas.getContext("2d");
+    const strip = document.querySelector(".strip");
+    const snap = document.querySelector(".snap");
+
+    function getVideo() {
+        navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(localMediaStream => {
+            // video.src = localMediaStream; // needs to be converted to URL to work (below)
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        });
+    }
+
+    getVideo();
+
+});
